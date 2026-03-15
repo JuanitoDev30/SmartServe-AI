d77c7131377871ccf30caf4f67dd99b0b23daec0
JuanitoDev30 <jsmolano@unimayor.edu.co>
2026-03-15 00:16:00 -0500

    Integrate Producto module and Dto

---
+++ Codigo/backend/src/producto/dto/create-producto.dto.ts
@@ -0,0 +1,39 @@
+//esta es la clase , que basicamente representa la informacion de como estoy esperando que luzca el producto a crear,
+// es decir, que campos va a tener, que tipo de datos se espera, etc.
+
+import {
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductoDto {
+  @IsString()
+  @MinLength(3) //el nombre del producto debe tener al menos 3 caracteres
+  nombre!: string; //el signo de exclamacion es para decirle a typescript que este campo es obligatorio, es decir, que no puede ser null o undefined
+
+  @IsNumber() //el precio del producto debe ser un numero
+  @IsPositive() //el precio del producto debe ser un numero positivo
+  @IsOptional() //el precio del producto es opcional, es decir, que puede ser null o undefined
+  precio?: number;
+
+  @IsString()
+  @IsOptional()
+  descripcion?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString()
+  @IsOptional()
+  proveedor?: string;
+}
---
+++ Codigo/backend/src/producto/entities/producto.entity.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Producto {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  nombre!: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  precio?: number;
+
+  @Column('text', {
+    nullable: true,
+  })
+  descripcion?: string;
+
+  @Column('text', {
+    unique: true, //No se van tener 2 slugs iguales / o dos productos iguales
+  })
+  slug?: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock?: number;
+
+  @Column('text', {
+    nullable: true,
+  })
+  imagen?: string;
+
+  //Proveedor
+  @Column('text', {
+    nullable: true,
+  })
+  proveedor?: string;
+}

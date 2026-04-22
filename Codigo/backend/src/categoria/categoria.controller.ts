8c897831a79720e7ae41fdc14b5ae4da20b83b95
martinezgrasantiago214 <santiagomartinezg@unicauca.edu.co>
2026-04-21 21:39:12 -0500

    categoria.controller

--- Codigo/backend/src/categoria/categoria.controller.ts
+++ Codigo/backend/src/categoria/categoria.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { CategoriaService } from './categoria.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
@@ -15,31 +16,36 @@ import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 export class CategoriaController {
   constructor(private readonly categoriaService: CategoriaService) {}
 
+  //  CREATE
   @Post()
   create(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriaService.create(createCategoriaDto);
   }
 
+  // GET ALL
   @Get()
   findAll() {
     return this.categoriaService.findAll();
   }
 
+  // GET ONE
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriaService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.categoriaService.findOne(id);
   }
 
+  // UPDATE
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCategoriaDto: UpdateCategoriaDto,
   ) {
-    return this.categoriaService.update(+id, updateCategoriaDto);
+    return this.categoriaService.update(id, updateCategoriaDto);
   }
 
+  // DELETE
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriaService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.categoriaService.remove(id);
   }
 }
